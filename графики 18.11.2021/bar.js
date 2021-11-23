e58c5530dcd33f3d8d7e9c610b285c72eb55c8a5
TanyaKoveshnikova <74663018+TanyaKoveshnikova@users.noreply.github.com>
2021-11-23 17:16:34 +0500

    25,11,2021

--- графики 18.11.2021/bar.js
+++ графики 18.11.2021/bar.js
@@ -4,10 +4,10 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Desktop-разработчик', 'Mobile разработчик', 'WEB - Разработчик', 'Data Scientist', 'GameDev', 'ERP - программист, Программист 1С', 'Системный интегратор', 'QA – инженер', 'Администратор баз данных', 'Системный программист', 'IoT-инженер', 'Системный администратор ИС (DevOps инженер)', 'UX/UI дизайнер', 'Менеджер IT-проекта', 'Аналитик', 'Специалист по интернет-маркетингу', 'Мусор'],
+        labels: ['Desktop-разработчик ', 'Mobile разработчик ', 'WEB - Разработчик ', 'Data Scientist ', 'GameDev ', 'ERP - программист, Программист 1С ', 'Системный интегратор ', 'QA – инженер ', 'Администратор баз данных ', 'Системный программист ', 'IoT-инженер ', 'Системный администратор ИС (DevOps инженер) ', 'UX/UI дизайнер ', 'Менеджер IT-проекта ', 'Аналитик ', 'Специалист по интернет-маркетингу ', 'Мусор'],
         datasets: [{
             label: 'value',
-            data: [2446, 20413, 72194, 2058, 1254, 75058, 41, 18180, 4067, 18391, 7, 111059, 2018, 13742, 1492, 3901, 11062],
+            data: [17275, 57486, 89478, 2585, 14383, 24448, 130, 99973, 6085, 27246, 30, 128542, 2879, 11980, 1954, 10556, 51265],
             backgroundColor:
                 'rgba(216, 27, 96, 0.6)',
             borderColor:
@@ -22,7 +22,7 @@ var myChart = new Chart(ctx, {
         },
         title: {
             display: true,
-            text: 'prof',
+            text: 'Количество вакансий в группах',
             position: 'top',
             fontSize: 16,
             padding: 20
@@ -35,9 +35,7 @@ var myChart = new Chart(ctx, {
             }]
         }
     }
-});
-
-
+}); //Количество вакансий в группах
 var ctx2 = document.getElementById('myChart2').getContext('2d');
 
 var myChart2 = new Chart(ctx2, {
@@ -61,7 +59,7 @@ var myChart2 = new Chart(ctx2, {
         },
         title: {
             display: true,
-            text: 'New prof',
+            text: 'Новые профессии',
             position: 'top',
             fontSize: 16,
             padding: 20
@@ -74,4 +72,79 @@ var myChart2 = new Chart(ctx2, {
             }]
         }
     }
-});
+});// Новые профессии
+
+var ctx3 = document.getElementById('myChart3').getContext('2d');
+
+var myChart3 = new Chart(ctx3, {
+    type: 'bar',
+    data: {
+        labels: ["Верстальщик", "front", "back", "Конструктор сайтов/cms", "Контент менеджмент", "Интернет маркетинг"],
+        datasets: [{
+            label: 'value',
+            data: [1706, 2229, 22810, 20, 1994, 504],
+            backgroundColor:
+                'rgba(216, 27, 96, 0.6)',
+            borderColor:
+                'rgba(216, 27, 96, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: false,
+        legend: {
+            display: false
+        },
+        title: {
+            display: true,
+            text: 'WEB - Разработчик',
+            position: 'top',
+            fontSize: 16,
+            padding: 20
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    min: 10
+                }
+            }]
+        }
+    }
+}); //Web - Программисты
+var ctx4 = document.getElementById('myChart4').getContext('2d');
+
+var myChart4 = new Chart(ctx4, {
+    type: 'bar',
+    data: {
+        labels: ['Аналитик', 'Программист-разработчик', 'Специалист по внедрению'],
+        datasets: [{
+            label: 'value',
+            data: [28568, 3180, 3415],
+            backgroundColor:
+                'rgba(216, 27, 96, 0.6)',
+            borderColor:
+                'rgba(216, 27, 96, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: false,
+        legend: {
+            display: false
+        },
+        title: {
+            display: true,
+            text: 'ERP - программист, Программист 1С',
+            position: 'top',
+            fontSize: 16,
+            padding: 20
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    min: 10
+                }
+            }]
+        }
+    }
+}); //Web - Программисты
