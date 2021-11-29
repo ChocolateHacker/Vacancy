895dede2f7a1479cbdcbfc6558de3675f2f2ef43
TanyaKoveshnikova <74663018+TanyaKoveshnikova@users.noreply.github.com>
2021-11-29 21:24:07 +0500

    для встречи 2,1,2021

--- графики 18.11.2021/bar.js
+++ графики 18.11.2021/bar.js
@@ -79,10 +79,10 @@ var ctx3 = document.getElementById('myChart3').getContext('2d');
 var myChart3 = new Chart(ctx3, {
     type: 'bar',
     data: {
-        labels: ["Верстальщик", "front", "back", "Конструктор сайтов/cms", "Контент менеджмент", "Интернет маркетинг"],
+        labels: ["web разработчик", "web-разработчик", "веб-разработчик", "веб-программист","веб",  "веб программист", "веб разработчик", "разработчик веб-приложений", "web-программист", "php программист", "php-разработчик","php разработчик", "php developer", "web", "разработчик wordpress", "веб-мастер", "web-мастер","html-верстальщик" ],
         datasets: [{
             label: 'value',
-            data: [1706, 2229, 22810, 20, 1994, 504],
+            data: [3316, 20655, 11286, 12123, 50378, 853,771, 267, 26654, 10049, 13332, 5051, 14898, 112029, 176, 2932, 4223, 11448],
             backgroundColor:
                 'rgba(241, 58, 19, 0.7)',
             borderColor:
@@ -117,10 +117,10 @@ var ctx4 = document.getElementById('myChart4').getContext('2d');
 var myChart4 = new Chart(ctx4, {
     type: 'bar',
     data: {
-        labels: ['Аналитик', 'Программист-разработчик', 'Специалист по внедрению'],
+        labels: ["программист 1с", "разработчик 1с", "программист-разработчик 1c", "1С ERP", "инженер-программист 1с", "erp-системы", "консультант erp", "sap erp", "Программист Битрикс", "Разработчик 1С-Битрикс", "PHP-разработчик Bitrix", "консультант 1c", "консультант-аналитик 1c", "аналитик 1c",  "программист-разработчик 1c", "инженер-программист 1c", "методист 1c",  "архитектор 1c", "специалист по внедрению 1c"],
         datasets: [{
             label: 'value',
-            data: [28568, 3180, 3415],
+            data: [111247, 9027, 136, 1457, 3377, 537, 322, 1152, 1510, 782, 24, 3307, 57, 349,  136, 352, 32,  67, 29],
             backgroundColor:
                 'rgba(65, 61, 81, 0.7)',
             borderColor:
@@ -155,10 +155,18 @@ var ctx5 = document.getElementById('myChart5').getContext('2d');
 var myChart5 = new Chart(ctx5, {
     type: 'bar',
     data: {
-        labels: ['Сетевой администратор', 'Разработчик'],
+        labels: [ "администратор", "сисадмин", "системный администратор",
+            "сетевой администратор", "администратор сетей", "системний адміністратор", "системный администратор linux",
+            "системный администратор серверов linux", "системный администратор linux-систем", "дежурный администратор linux",
+            "специалист по внедрению", "системный инженер", "инженер devops", "devops-инженер", "devops инженер", "sre инженер",
+            "системный инженер linux", "специалист devops","devops администратор", "devops-разработчик",
+            "devops administrator", "linux devops", "linux administrator", "administrator", "systems development engineer",
+            "sre engineer", "linux engineer", "system administrator", "sysadmin", "specialist devops", "devops specialist",
+            "specialist linux", "platform engineer", "devops engineer", "devops engineer hadoop",  "azure devops engineer", "devops specialist",
+            "cloud devops specialist"],
         datasets: [{
             label: 'value',
-            data: [11721, 29099],
+            data: [238956, 77, 165102, 2737, 276, 501, 6349, 10, 5, 4, 9624, 9610, 884, 1177, 3510, 41, 278, 102, 15, 7, 7, 98, 280, 8937, 789, 45, 204, 4325, 86, 32, 945, 2, 133, 8923, 1, 25, 945, 6],
             backgroundColor:
                 'rgba(76, 187, 23, 0.7)',
             borderColor:
