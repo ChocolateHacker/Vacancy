10b2401af76de4652ee253195103bc3feec7c82f
TanyaKoveshnikova <74663018+TanyaKoveshnikova@users.noreply.github.com>
2021-12-15 21:48:16 +0500

    переделано

--- проги.тексты 18,11 ++/for_all_value.py
+++ проги.тексты 18,11 ++/for_all_value.py
@@ -6,7 +6,6 @@ data = literal_eval(file)
 for key in data.keys():
     valueX = []
     numberY = []
-    counter = 0
     array_in_prof = data.get(key)
     # with open("profs_stats.csv", encoding='utf-8') as r_file:
     #     file_reader = csv.reader(r_file, delimiter=",")
@@ -18,6 +17,7 @@ for key in data.keys():
     # valueX.append(f'{key}\n{counter}')
     # numberY.append(counter)
     for i in array_in_prof:
+        counter = 0
         with open("filtredbaza.csv", encoding='utf-8') as r_file:
             file_reader = csv.reader(r_file, delimiter=",")
             for row in file_reader:
