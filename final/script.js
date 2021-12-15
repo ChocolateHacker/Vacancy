fa548442a803610c495910e618c9beab6acc505e
Tanya Koveshnikova <T.A.Koveshnikova@urfu.me>
2021-12-15 19:14:25 +0500

    динам страничка(изменяем графики)

--- final/script.js
+++ final/script.js
@@ -1,1 +1,12 @@
-//Тут напишем динамическую смену графиков
+//Логика переключения графиков
+let select = document.getElementById('select');
+let block = document.querySelectorAll('.d');
+let lastIndex = 0;
+select.addEventListener('click', function() {
+    block[lastIndex].style.display = "none";
+    let index = select.selectedIndex;
+    block[index].style.display = "block";
+    lastIndex = index; // Обновить сохраненный индекс.
+});
+
+
