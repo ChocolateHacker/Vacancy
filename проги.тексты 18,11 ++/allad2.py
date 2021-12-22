ba06e93323238ac0f2a34e0fe42294d19722968f
TanyaKoveshnikova <74663018+TanyaKoveshnikova@users.noreply.github.com>
2021-12-22 22:48:05 +0500

    Add files via upload

--- проги.тексты 18,11 ++/allad2.py
+++ проги.тексты 18,11 ++/allad2.py
@@ -6,24 +6,23 @@ counter = 0
 file = open("prof_data_value_2.txt", "r", encoding='utf-8').read()
 data = literal_eval(file)
 for key in data.keys():
-
     counter = 0
     array_in_prof = data.get(key)
     with open("profs_stats.csv", encoding='utf-8') as r_file:
         file_reader = csv.reader(r_file, delimiter=",")
         for row in file_reader:
+            if len(row) == 0:
+                continue
             for i in array_in_prof:
                 if i.lower() == row[3]:
-
                     counter += int(row[0])
-    valueX.append(f'{key}\n{counter}')
+    valueX.append(f'{key} {counter}')
     numberY.append(counter)
-
 
     # with open("filtredbaza.csv", encoding='utf-8') as r_file:
     #     file_reader = csv.reader(r_file, delimiter=",")
     #     for row in file_reader:
-    #         # for i in array_in_prof:
+    #         for i in array_in_prof:
     #             if i.lower() in row[0]:
     #                 counter += int(row[1])
     # with open("filtredbazaAnal.csv", encoding='utf-8') as r_file:
@@ -32,7 +31,10 @@ for key in data.keys():
     #         for i in array_in_prof:
     #             if i.lower() in row[0]:
     #                 counter += int(row[1])
+    #
+    # valueX.append(f'{key} {counter}')
+    # numberY.append(counter)
 
-print( "HEllo")
+print("HEllo")
 print(valueX)
 print(numberY)
